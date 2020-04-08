79eacf53c0424b04c313bf45cf2d0a23e66e9703
shrey <shreyviradiya@gmail.com>
2020-04-08 13:33:28 +0530

    CSV file cleaning

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from models.BooksData import BooksData
 
 Data = BooksData()
 
-print(Data.loadBooksData())
-# print(Data.getUserRatings(35433))
+Data.loadBooksData()
+# Data.getUserRatings(35433)
+x = Data.getPopularityRanks()
+
+for isbn in list(x.keys())[:15]:
+    try:
+        print(Data.ISBN_to_title[isbn])
+    except KeyError:
+        print(f"No Book data found for {isbn}")
