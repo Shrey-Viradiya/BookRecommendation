516bb8b403fbfbe88cfda525a4ca1828a2061b1c
shrey <shreyviradiya@gmail.com>
2020-04-08 12:07:54 +0530

    DataCleaning

--- data/datacleaning.py
+++ data/datacleaning.py
@@ -4,7 +4,14 @@ with open('ratings.csv', newline='\n', encoding='utf-8') as csvfile, open('ratin
     wrt = csv.writer(userfile, delimiter=',')
     for row in rdr:
         try:
-            x = float(row[2])
+            x = float(row[3])
             wrt.writerow([row[x] for x in range(1,4)])
         except:
             pass
+
+# import pandas as pd
+#
+# df = pd.read_csv('BX-Book-Ratings.csv', delimiter=';')
+# print(df)
+#
+# df.to_csv('ratings.csv', sep=',')
